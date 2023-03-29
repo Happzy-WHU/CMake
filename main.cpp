05f8a3e81277164624cef7547b9c344352d1d0b7
Happzy-WHU <702146903@qq.com>
2023-03-29 16:12:23 +0800

    cmake 例子

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+    cout<<123;
+}
